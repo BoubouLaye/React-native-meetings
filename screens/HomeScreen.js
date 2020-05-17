c97fb2a62db7a43f8b8a40dd9cdf8514bd178b36
boubs <bndiaye0799@gmail.com>
2020-05-17 06:19:46 +0100

    date ok

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native';
+import { Icon } from 'react-native-elements'
 
 export default function HomeScreen() {
 
@@ -8,17 +9,17 @@ export default function HomeScreen() {
 
 
     <View style={styles.container}>
-     
-        <SafeAreaView style={styles.container}>
-         
-          <FlatList
-            data={Reuinions}
-            style={styles.liste}
-            keyExtractor={item => item.id}
-            renderItem={({ item }) => <Item title={item.title} />}
-          />
-        </SafeAreaView> 
-      
+
+      <SafeAreaView style={styles.container}>
+
+        <FlatList
+          data={Reuinions}
+          style={styles.liste}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <Item reunion={item} />}
+        />
+      </SafeAreaView>
+
     </View>
 
 
@@ -32,42 +33,50 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'#111d4a'
+    backgroundColor: '#111d4a'
 
 
   },
   item: {
-    
+
     backgroundColor: '#111d4a',
-    borderWidth:3,
+    borderWidth: 3,
     borderColor: '#e63946',
     borderWidth: 1,
     padding: 10,
     marginVertical: 4,
     marginHorizontal: 16,
     borderRadius: 7
-    
+
 
   },
   title: {
-    fontSize: 25,
-    color:'#fff8f0'
+   
+    color: '#fff8f0'
   },
   liste: {
-    marginTop:15
+    marginTop: 15
   },
 
- 
-  
+
+
 });
 
 
-function Item({ title }) {
+function Item({ reunion }) {
   return (
 
     <View style={styles.item}>
 
-      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.title}>{reunion.title}</Text>
+      <Text style={styles.title}>Date : {reunion.date}</Text>
+      <Text style={styles.title}>Salle : {reunion.salle}</Text>
+      <View style={{alignItems: 'flex-end',justifyContent:'center'}}>
+      <Icon
+        name='delete'
+        color='red'        
+      />
+      </View>
     </View>
   );
 }
@@ -76,23 +85,37 @@ const Reuinions = [
   {
     id: '1',
     title: 'echanges commerciaux',
+    date: '17 april 2020',
+    salle: '2',
+    participants: ''
+
   },
   {
     id: '2',
     title: 'reconstitution du personnel',
+    date: '17 april 2020',
+    salle: '2',
+    participants: ''
   },
   {
     id: '3',
     title: 'payement des factures',
+    date: '17 april 2020',
+    salle: '2',
+    participants: ''
   },
   {
     id: '4',
     title: 'emploi du temps',
+    date: '17 april 2020',
+    salle: '2',
+    participants: ''
   },
   {
     id: '5',
     title: 'mr gauthier',
+    date: '17 april 2020',
+    salle: '2',
+    participants: ''
   },
 ];
-
-
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,5 +1,9 @@
 import * as React from 'react';
-import { StyleSheet, Text,   View, TextInput, Dimensions, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Dimensions, TouchableOpacity } from 'react-native';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import moment from "moment";
+import Reuinions from './HomeScreen'
+
 var width = Dimensions.get('window').width; //full width
 
 
@@ -8,41 +12,83 @@ export default class LinksScreen extends React.Component {
   constructor() {
     super()
     this.state = {
-      isVisible : false,
-      choosenDate:''
+      isVisible: false,
+      choosenDate: ''
     }
-  }
- 
-    
-     
-  render() {
-  return (
-    <View style={styles.container}>       
-       <Text style={styles.titre}>Merci de renseigner les champs suivants</Text>
-          <TextInput
-            placeholder='Lieu'          
-            style={styles.simpleInput}
-            />  
-        
-          <TextInput
-            placeholder='Sujet de la reuinion'
-            style={styles.simpleInput}
-            />
 
-          
-          <TextInput
-            placeholder='liste des participants '
-            multiline = {true}
-            numberOfLines = {4}
-            style={styles.textAreaInput}
-            />    
-          <TouchableOpacity
-            style={styles.buttonSave}>
-            <Text style={styles.txtbutton}>Enregistrer</Text>
-          </TouchableOpacity>
+
+  }
+  handlePicker = (datetime) => {
+    this.setState({
+      isVisible: false,
+      choosenDate: moment(datetime).format(' D MMMM YYYY, HH:mm')
+    })
+  }
+  hidePicker = () => {
+    this.setState({
+      isVisible: false,
+
+    })
+  }
+  showPicker = () => {
+    this.setState({
+      isVisible: true
+    })
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <Text style={styles.titre}>Merci de renseigner les champs suivants</Text>
+
+        <TextInput
+          placeholder='Lieu'
+          style={styles.simpleInput}
+        />
+
+        <DateTimePickerModal
+          isVisible={this.state.isVisible}
+          onConfirm={this.handlePicker}
+          onCancel={this.hidePicker}
+          mode="datetime"
+          is24Hour={false}
+        />
+        <TouchableOpacity
+          style={styles.buttonDate}
+          onPress={this.showPicker}>
+          <Text style={styles.date}>choisir la date</Text>
+        </TouchableOpacity>
+
+        <Text style={{ color: '#ffcf99', fontSize: 20 }}>
+          {this.state.choosenDate}
+        </Text>
+
+        <TextInput
+          placeholder='Sujet de la reuinion'
+          style={styles.simpleInput}
+        />
+
+        <TextInput
+          placeholder='liste des participants '
+          multiline={true}
+          numberOfLines={4}
+          style={styles.textAreaInput}
+        />
+
+        <TouchableOpacity
+          style={styles.buttonSave}>
+          <Text style={styles.txtbutton}>Enregistrer</Text>
+        </TouchableOpacity>
+
+
       </View>
-  );
-}}
+    );
+  }
+}
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -53,34 +99,34 @@ const styles = StyleSheet.create({
   simpleInput: {
     width: width - 40,
     marginTop: 30,
-    borderRadius:7,
-    height: 40, 
-    borderColor: '#ffcf99', 
-    
-    paddingHorizontal: 10, 
+    borderRadius: 7,
+    height: 40,
+    borderColor: '#ffcf99',
+
+    paddingHorizontal: 10,
     borderWidth: 1
   },
 
   buttonSave: {
-    
-    marginTop:40,
+
+    marginTop: 40,
     backgroundColor: '#92140c',
     borderRadius: 20,
-    borderWidth:3,
-    borderColor:'#fff8f0',
+    borderWidth: 3,
+    borderColor: '#fff8f0',
     width: 150,
     height: 50,
     alignItems: 'center',
     justifyContent: 'center'
-    
+
   },
-  txtbutton:{
-    color:'#fff8f0',
-    fontSize:18,
+  txtbutton: {
+    color: '#fff8f0',
+    fontSize: 18,
   },
   buttonDate: {
-    marginTop:10,
-    
+    marginTop: 10,
+
     borderRadius: 20,
     width: 100,
     height: 40,
@@ -88,21 +134,29 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   textAreaInput: {
-    borderRadius:7,
+    borderRadius: 7,
     width: width - 40,
     marginTop: 30,
-    height: 100, 
-    borderColor: '#ffcf99', 
+    height: 100,
+    borderColor: '#ffcf99',
     borderWidth: 1,
     paddingHorizontal: 10,
     marginBottom: 30
   },
   titre: {
-    marginTop :30,
+    marginTop: 30,
     marginBottom: 25,
-    fontSize:20,
-    color:'#ffcf99'
-   
+    fontSize: 20,
+    color: '#ffcf99'
+
   },
-  
+  date: {
+    fontSize: 20,
+    color: '#ffcf99',
+    width: 150
+
+  }
+
 });
+
+
